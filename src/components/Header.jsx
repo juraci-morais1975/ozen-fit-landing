f4fac8969191fe836a3eba71c308ae2841d17b5b
juraci-morais1975 <adryribeirobeautyecare@gmail.com>
2026-03-15 06:07:40 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,16 +10,4 @@ style={{width:"380px",margin:"20px auto"}}
   )
 }
 
-function Header() {
-  return (
-    <header style={{background:"#fff", color:"#fff"}}>
-      <img
-src="/logo1.png"
-alt="logo"
-style={{width:"380px",margin:"20px auto"}}
-/>
-    </header>
-  )
-}
-
 export default Header
